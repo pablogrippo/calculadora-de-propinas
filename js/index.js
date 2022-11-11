0d4a82278b852f0450fea592350441e26616f963
Pablo Grippo <pablogrippo@gmail.com>
2022-11-11 16:39:03 -0300

    Se agrego localStorage y el uso de la libreria luxon al proyecto

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Guardo mi formulario en una constante
 const calculadoraDePropinas = document.getElementById("miFormulario");
 
 //Funcion para calcular la propina
@@ -10,7 +11,8 @@ calculadoraDePropinas.addEventListener("submit", (event) => {
             this.nombre = nombre.toUpperCase();
             this.apellido = apellido.toUpperCase();
         }
-    
+
+        // Funcion para mostrar en pantalla el nombre y apellido del usuario. 
         saludar() {
             if (this.nombre == "" && this.apellido == "") {
                 document.getElementById('saludo').innerHTML = "";
@@ -32,6 +34,10 @@ calculadoraDePropinas.addEventListener("submit", (event) => {
     const porcentaje = porcentajePropina / 100;
     const resultado1 = totalCompra * porcentaje;
     const resultado2 = resultado1 / totalPersonas;
+
+    // Uso de localStorage para guardar el calculo de propina en el navegador. 
+    localStorage.setItem("calculoPropina", resultado1);
+    totalPropina = JSON.parse(localStorage.getItem("calculoPropina"));
     
     if (totalCompra == "") {
         document.getElementById('r3').innerHTML = "INTRODUZCA TODOS LOS DATOS";
@@ -39,8 +45,13 @@ calculadoraDePropinas.addEventListener("submit", (event) => {
         document.getElementById('r2').innerHTML = ":(";
     } else {
         //Respuestas
-        document.getElementById('r1').innerHTML = resultado1.toFixed(2);
+        document.getElementById('r1').innerHTML = totalPropina.toFixed(2);
         document.getElementById('r2').innerHTML = resultado2.toFixed(2);
     }
-
 });
+
+// Utilización de la libreria Luxon para mostrar el año en el cual fue creada esta app. 
+const DateTime = luxon.DateTime;
+const hoy = DateTime.now();
+
+document.getElementById('anio').innerHTML = hoy.year;
