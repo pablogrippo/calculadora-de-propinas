077be5ffe6393b6dddc68bfc41b6a6f6e42e606d
Pablo Grippo <pablogrippo@gmail.com>
2022-11-18 12:36:12 -0300

    Se agrego algo mas de asincronia

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // Guardo mi formulario en una constante
 const calculadoraDePropinas = document.getElementById("miFormulario");
 
+// Guardo mi contenedor en una constante
+const contenedor = document.getElementById("contenedor");
+
 // Creando una clase Persona. 
 class Persona {
 
@@ -16,6 +19,17 @@ class Persona {
         } else {
             document.getElementById('saludo').innerHTML = `HOLA  ${this.nombre}  ${this.apellido}`;
         }
+    }
+}
+
+function bloquearBoton (bloqueado = true) {
+
+    const boton = document.getElementById("btn_calcular");
+
+    if(bloqueado) {
+        boton.disabled = "true";
+    } else {
+        boton.disabled = "";
     }
 }
 
@@ -39,7 +53,51 @@ calculadoraDePropinas.addEventListener("submit", (event) => {
 
     // Uso de localStorage para guardar el calculo de propina en el navegador. 
     localStorage.setItem("calculoPropina", porcentaje);
-    calculoUltimaPropina = JSON.parse(localStorage.getItem("calculoPropina"));
+    calculoUltimaPropina = JSON.parse(localStorage.getItem("calculoPropina"));    
+
+    function enviarFormulario(totalCompra, porcentajePropina, totalPersonas) {
+
+        return new Promise( (resolve, reject) => {
+    
+            // Bloqueo del boton
+            bloquearBoton();
+    
+            const random = Math.round(Math.random()); 
+    
+            setTimeout( () => {
+    
+                if(random === 1) {
+                    resolve("Mensaje enviado con éxito");
+                } else {
+                    reject("Error en el servidor");
+                }
+            }, 1000);
+        });
+    }
+
+    // Envio del formulario mediante un request 
+    enviarFormulario(totalCompra, porcentajePropina, totalPersonas).then( (mensaje) => {
+
+            contenedor.innerHTML = `<strong style="color: green">${mensaje}</strong>`;
+
+            // Oculto el formulario
+            miFormulario.style.display = "none"; 
+
+        }).catch( (errorFormulario) => {
+
+            contenedor.innerHTML = `<strong style="color: red">${errorFormulario}</strong>`
+
+            document.getElementById('nombre').value = "";
+            document.getElementById('apellido').value = "";
+            document.getElementById('box1').value = "";
+            document.getElementById('box2').value = "";
+            document.getElementById('box3').value = "";
+
+        }).finally( () => {
+
+            bloquearBoton(false);
+
+        });
 
     if (totalCompra === "") {
         document.getElementById('r3').innerHTML = "INTRODUZCA TODOS LOS DATOS";
@@ -53,11 +111,10 @@ calculadoraDePropinas.addEventListener("submit", (event) => {
     }
 });
 
-// Utilización de la libreria Luxon. 
+// Utilización de la libreria Luxon para mostrar el año en el cual fue creada esta app.
 const dateTime = luxon.DateTime;
 const hoy = dateTime.now();
-
-// Uso de Asincronía para mostrar el año en el cual fue creada esta app. 
+ 
 setTimeout( () => {
     document.getElementById('anio').innerHTML = hoy.year;
 }, 500)
